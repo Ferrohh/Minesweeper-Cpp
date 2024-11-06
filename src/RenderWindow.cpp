542ec0c081c939b872202d5dac7338e5f19f5d55
Ferroh <andyferrari5768@gmail.com>
2024-11-06 16:30:12 +0100

    random fixes

--- src/RenderWindow.cpp
+++ src/RenderWindow.cpp
@@ -9,7 +9,7 @@ RenderWindow::RenderWindow(std::string p_title, int p_w, int p_h)
         std::cerr  << "Window failed to init! Error: " << SDL_GetError() << std::endl;
     }
 
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
 }
 
 RenderWindow::~RenderWindow() 
